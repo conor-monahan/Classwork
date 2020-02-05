442ac5e76dcb0756f9e64e1608121be1cee06648
conor-monahan <55504719+conor-monahan@users.noreply.github.com>
2020-02-05 08:53:11 -0800

    Add files via upload

---
+++ ArrayListProcessing.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class ArrayListProcessing
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> sports = new ArrayList<String>();
+        sports.add("Softball");
+        sports.add("Soccer");
+        sports.add("Basketball");
+        sports.add("Tennis");
+        sports.add("Soccer");
+        sports.add("Skiing");
+        sports.add("Rowing");
+        
+        System.out.println("Question 3:" + returnElement(sports, 3));
+        System.out.println("Question 4:");
+        removeThirdElement(sports);
+    }
+    
+    // Number 3
+    public static String returnElement(ArrayList<String> inList,int i)
+    {
+        return inList.get(i);
+    }
+    
+    // Number 4
+    public static void removeThirdElement(ArrayList<String> inList)
+    {
+        inList.remove(2);
+        for(int i = 0; i < inList.size(); i++)
+            System.out.println(inList.get(i));
+    }
+    
+    // Number 1
+    public static void printElements(ArrayList<String> inList)
+    {
+        for(int i = 0; i<inList.size(); i++)
+            System.out.println(inList.get(i));
+    }
+    
+    //Number 2
+    public static void addElements(ArrayList<String> inList)
+    {
+        inList.add("chess");
+        for(int i = 0; i<inList.size(); i++)
+            System.out.println(inList.get(i));
+    }
+    
+    public static void findElements(ArrayList<String> inList, String thing)
+    {
+        for(int i = 0; i<inList.size(); i++)
+        {
+            if(thing == inList(int i))
+            {
+                System.out.println(inList.get(i));
+            }
+        }
+    }
+}
